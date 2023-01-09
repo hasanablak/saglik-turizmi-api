e661f4069138b80e60b267a96543cfaf10752956
Hasan Ablak <0hasanablak@gmail.com>
2023-01-09 10:56:11 +0300

    endpointler

--- routes/api.php
+++ routes/api.php
@@ -1,60 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-	return $request->user();
-});
-
-
-Route::post('/tokens/create', function (Request $request) {
-	$token = $request->user()->createToken($request->token_name);
-
-	return ['token' => $token->plainTextToken];
-});
-
-Route::post('/sanctum/token', function (Request $request) {
-	$request->validate([
-		'email' => 'required|email',
-		'password' => 'required',
-		'device_name' => 'required',
-	]);
-
-	$user = User::where('email', $request->email)->first();
-
-	if (!$user || !Hash::check($request->password, $user->password)) {
-		throw ValidationException::withMessages([
-			'email' => ['The provided credentials are incorrect.'],
-		]);
-	}
-
-	return $user->createToken($request->device_name)->plainTextToken;
-});
+use App\Http\Controllers\TransportController;
+use App\Http\Controllers\UserController;
 
 
 Route::controller(AuthController::class)->group(function () {
 	Route::post('/login', 'login');
 	Route::post('/register', 'register');
-	Route::post('/logout', 'logout')->middleware('auth:api');
+	Route::post('/logout', 'logout')->middleware('auth:sanctum');
 });
 
-Route::resource("cars", CarController::class)
-	->middleware('auth:sanctum')
-	->except(["create"]);
+Route::middleware("auth:sanctum")->group(function () {
+
+	Route::resource("cars", CarController::class)
+		->except(["create", "edit"]);
+
+	Route::resource("users", UserController::class)
+		->except(["create", "edit"]);
+
+
+	Route::resource("transports", TransportController::class)
+		->except(["create", "edit"]);
+});
